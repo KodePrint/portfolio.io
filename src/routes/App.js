71f91e26415fb4e8faf1cc21a60bdc237c50dcf9
Kevin Alberto Palma Ralda <kapalma05@gmail.com>
2022-06-27 22:28:12 -0600

    :sparkles: create home structure
    create home structure

--- src/routes/App.js
+++ src/routes/App.js
@@ -6,18 +6,54 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="home-container container">
-        <h1>Buiding a solutions for the future..!</h1>
-      </div>
-      <div className="about-container container">
+      <section id='home' className="home-container container">
+        
+        <div className="info">
+          <h3>Buiding a solutions with tecnology..!</h3>
+          <h1>
+            Kevin Palma
+          </h1>
+          <h4>
+            web developer, software engineer, and a full stack developer.
+          </h4>
+        </div>
+        
+        {/* <button className='slider-btn left'>L</button>
+        <button className='slider-btn rigth'>R</button> */}
+        <div className="slider-container">
+          
+          <div className="proyects">
+
+            <div className="card">
+              <h3>Proyecto</h3>
+            </div>
+            <div className="card">
+              <h3>Proyecto</h3>
+            </div>
+            <div className="card">
+              <h3>Proyecto</h3>
+            </div>
+            <div className="card">
+              <h3>Proyecto</h3>
+            </div>
+            <div className="card">
+              <h3>Proyecto</h3>
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+      <section id='about' className="about-container container">
         <h1>About</h1>
-      </div>
-      <div className="porfolio-container container">
+      </section>
+      <section id='portfolio' className="porfolio-container container">
         <h1>Portfolio</h1>
-      </div>
-      <div className="concat-container container">
+      </section>
+      <section id='contact' className="concat-container container">
         <h1>Contact</h1>
-      </div>
+      </section>
     </div>
   );
 }
